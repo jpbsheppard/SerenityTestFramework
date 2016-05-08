f9ad1d6f0d431c12ec4cc445fe2d13508c7bae66
jsheppard <jsheppard@amido.com>
2016-05-08 23:43:09 +0100

    TEST WORKING COMMIT

--- src/test/java/tests/serenityTests/LoginToHPe.java
+++ src/test/java/tests/serenityTests/LoginToHPe.java
@@ -1,11 +1,13 @@
 package tests.serenityTests;
 
+
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.Title;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
 import serenity.sceneriosteps.loginPageScenarioSteps;
 
 
@@ -16,6 +18,7 @@ import serenity.sceneriosteps.loginPageScenarioSteps;
 public class LoginToHPe {
 
     @Managed(driver = "Firefox")
+    WebDriver driver;
 
     @Steps
     loginPageScenarioSteps user;
